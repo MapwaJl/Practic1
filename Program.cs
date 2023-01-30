761b34543522656953a5c8f1f3b3e3e59ffb9a14
MapwaJl <serjo.malyshko@example.com>
2023-01-30 14:54:22 +0300

    Задача на практическом занятии

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿// Напишите программу, которая на вход принимает число и выдает его квадрат.
+
+Console.Write("Введи число: ");
+int number = int.Parse(Console.ReadLine()!);
+
+// Решение математически
+
+int num = number * number;
+Console.WriteLine($"Квадрат числа {number} равен {num}");
+
+// Решение через библиотеку
+
+int num2 =Convert.ToInt32(Math.Pow(number, 2));
+Console.WriteLine($"Квадрат числа {number} равен {num}");
